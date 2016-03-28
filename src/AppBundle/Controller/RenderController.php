094fac788ffe7b2f4ae0c8048ca5e0eba7991ae6
Julien Sanchez <sumbobyboys@gmail.com>
2016-03-29 00:05:47 +0200

    print test from iphone

--- src/AppBundle/Controller/RenderController.php
+++ src/AppBundle/Controller/RenderController.php
@@ -3,7 +3,10 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\HttpFoundation\Response;
 
 class RenderController extends Controller
@@ -17,5 +20,26 @@ class RenderController extends Controller
         $cardRegistry = $this->container->get('journal.registry.card');
 
         return new Response($journalRenderer->render($cardRegistry->getCards()));
+    }
+
+    /**
+     * @Route("/print", name="print")
+     */
+    public function printAction()
+    {
+        $kernel = $this->container->get('kernel');
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput([
+           'command' => 'journal:print'
+        ]);
+
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+
+        $content = $output->fetch();
+
+        return new Response($content);
     }
 }
