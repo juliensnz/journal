d51fcea700a2d1cd223bb8cb6e454d9c8f503506
Julien Sanchez <sumbobyboys@gmail.com>
2016-04-17 16:21:58 +0200

    test

--- app/Resources/python/print.py
+++ app/Resources/python/print.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
 from Adafruit_Thermal import *
-# import Image
+import Image
 import sys
-printer = Adafruit_Thermal()
+printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
 
 # printer.wake()       # Call wake() before printing again, even if reset
 # printer.printImage(Image.open(sys.argv[1]), True)
-printer.printImage("/Users/juliensanchez/Desktop/test_python.png", True)
+printer.printImage("/home/pi/test_python.png", True)
 # printer.feed(3)
 # printer.sleep()      # Tell printer to sleep
 # printer.setDefault() # Restore printer to defaults
